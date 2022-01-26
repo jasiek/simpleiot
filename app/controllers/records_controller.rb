95f0058a2c15d17c105176c99393c300637ad871
Jan Szumiec <jan.szumiec@gmail.com>
2022-01-26 19:09:22 +0100

    Routing and display last record.

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,19 +1,24 @@
 class RecordsController < ApplicationController
   TOKEN = ENV["AUTH_TOKEN"]
 
-  before_filter :authenticate, except: [ :index ]
+  before_action :authenticate, except: [ :index ]
   
   def index
-    @records = Record.today
+    @record = Record.last
   end
 
   def create
-    
+    @record = Record.create!(record_params)
+    render status: :created
   end
 
   def authenticate
     authenticate_or_request_with_http_token do |token, options|
       ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
     end
+  end
+
+  def record_params
+    params.require(:record).permit(:value)
   end
 end
